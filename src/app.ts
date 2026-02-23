4d52fd3de6b4e8fa4ae2622812249d1de5b75ac5
boon2187 <fumiya.mai.ito@gmail.com>
2026-02-23 10:44:38 +0900

    エントリーポイントを実装似合わせて修正

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,16 @@
 import express from "express";
+import { BookController } from "./presentation/bookController.js";
 
 const app = express();
 
 app.use(express.json());
 
+const bookController = new BookController();
+
 const PORT = process.env.PORT || 3000;
+
+app.post("/books", bookController.add.bind(bookController));
+app.get("/books/:id", bookController.findById.bind(bookController));
 
 app.get("/", (req, res) => {
   res.json({ message: "Hello, World!" });
